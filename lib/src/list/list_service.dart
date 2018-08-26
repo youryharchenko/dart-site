b407f8138e53c9873cb1790d1b31813b7b7cb285
Youry <youry.in.ua@gmail.com>
2018-08-26 15:25:12 +0300

    box2d added

--- lib/src/list/list_service.dart
+++ lib/src/list/list_service.dart
@@ -12,6 +12,11 @@ class ListService {
       "Learning WebGL in Dart", "https://youryharchenko.github.io/site/webgl/", 
       "Forked from", "https://github.com/jtmcdole/dart-webgl", 
       "Adapted to Dart2 (In progress)", "https://github.com/youryharchenko/dart-webgl"
+    ),
+    Item(2, 
+      "Box2d in Dart", "https://youryharchenko.github.io/site/box2d/", 
+      "Forked from", "https://github.com/google/box2d.dart", 
+      "Adapted to Dart2 (In progress)", "https://github.com/feroult/box2d.dart"
     )
   ];
   Future<List<Item>> getList() async => mockList;
